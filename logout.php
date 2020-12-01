b4f962bb514338214ca8a111e5c8af9e76b66e39
enggShanu <sanu16236@gmail.com>
2020-12-01 07:55:10 +0530

    Upload all file to remote server

---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php 
+include('dbconnection.inc.php');
+session_start();
+unset($_SESSION['ulogin']);
+header('location:index.php');
+
+?>
---
+++ warning.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Warning...</title>
+				<!-- bootstrap file  -->
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+					<style>
+  body{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    width:100%;
+    height:100vh;
+    background-image:url('./img/warning.jpg');
+    background-size:cover;
+    background-position:center;
+    background-repeat:no-repeat;
+  }
+  .ericon{
+    border-radius:50%;
+  }
+  </style>
+				</head>
+				<body class="d-flex justify-content-center align-items-center flex-column">
+					<div>
+						<img src="./img/ericon.png" class="img-fluid ericon mb-4" alt="">
+						</div>
+						<h3 class="text-center p-2 alert-warning alert mx-2">For See More Details Signin Or Signup First ?</h3>
+						<div class="text-center">
+							<a href="index.php" class="btn btn-lg btn-danger">Go to home</a>
+						</div>
+					</body>
+				</html>
